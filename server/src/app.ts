97602b8253e5bc4e9992d639fddae8b7936274e6
Justin-K-Ellis <jlke45@gmail.com>
2025-05-26 22:03:40 +0900

    refactor: continue boostrapping, TS configuring

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("../../client/dist"));
+app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
+  console.log(`Server listening on port ${PORT}.`);
 });
