c774e235ab68ba8ada53d7862de1373e71b84e0d
Justin-K-Ellis <jlke45@gmail.com>
2025-05-27 15:16:59 +0900

    refactor: replace App component w/ router in main.tsx
    Using React Router makes a App.tsx file unnecessary (and this is how it's done in the docs). I've also added
    a few basic routes to the home page and dynamic routes.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
+import EventDetails from "./pages/EventDetails";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="event">
+          <Route path=":id" element={<EventDetails />} />
+        </Route>
       </Route>
     </Routes>
   </BrowserRouter>
