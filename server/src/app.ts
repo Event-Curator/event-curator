1aea559cdd524a3822c3651488754130a0acb0de
Justin-K-Ellis <jlke45@gmail.com>
2025-05-28 11:01:08 +0900

    fix: fix package.json dependency & script issues
    The build was not able to complete due to discrepencies in the modules dependencies.
    This has been addressed by updating the package.json file to include apiche and
    nodemon as a dev dependency. The dev script has also been updated.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { log } from './utils/logger'
-import config from './utils/config';
-import eventRoute from './routes/eventRoutes';
-import { errorHandler } from './middlewares/errorHandler';
+import { log } from "./utils/logger.js";
+import config from "./utils/config.js";
+import eventRoute from "./routes/eventRoutes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use(express.static("../client/dist"));
-app.use('/api', eventRoute);
+app.use("/api", eventRoute);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
--- server/src/routes/eventRoutes.ts
+++ server/src/routes/eventRoutes.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
-import { getEvents } from '../controllers/eventController';
-import apicache from 'apicache';
+import { Router } from "express";
+import { getEvents } from "../controllers/eventController.js";
+import apicache from "apicache";
 
 const router = Router();
 let cache = apicache.middleware();
 
 // router.get('/events', cache('5 minutes'), getEvents);
-router.get('/events', getEvents);
+router.get("/events", getEvents);
 
 export default router;
