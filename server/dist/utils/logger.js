5b4c46b6bb44d8c5361a00606dd82a1190d7ce0b
Frédéric Beuserie <fbeuserie@gmail.com>
2025-05-28 17:23:37 +0900

    fix: server/dist subfolder removed. not needed

--- server/dist/utils/logger.js
+++
@@ -1,8 +0,0 @@
-import { createLogger, transports, format } from 'winston';
-const log = createLogger({
-    transports: [new transports.Console()],
-    format: format.combine(format.colorize(), format.timestamp(), format.printf(({ timestamp, level, message }) => {
-        return `[${timestamp}] ${level}: ${message}`;
-    }))
-});
-export { log };
