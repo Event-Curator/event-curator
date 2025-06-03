a3af03fc1c65eeddf6575b504a0b652772211a00
Justin-K-Ellis <jlke45@gmail.com>
2025-06-03 17:23:16 +0900

    refactor: wrap router in App component to use Context API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
-import Layout from "./layouts/Layout";
-import Home from "./pages/Home";
-import EventDetails from "./pages/EventDetails";
-import EventTimeline from "./pages/EventTimeline";
-//import { EventProvider } from "./context/EventContext";
+import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  //<EventProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="event">
-            <Route path=":id" element={<EventDetails />} />
-          </Route>
-          <Route path="timeline" element={<EventTimeline />} /> 
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  //</EventProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
