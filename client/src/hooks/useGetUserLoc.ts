1e4e8a626c703d3e4f9152e858e6ec71a8fe1a09
Justin-K-Ellis <jlke45@gmail.com>
2025-06-09 16:30:46 +0900

    fix: hook now shares info about if user refused to share location

--- client/src/hooks/useGetUserLoc.ts
+++ client/src/hooks/useGetUserLoc.ts
@@ -1,12 +1,19 @@
 import { useState } from "react";
+import type { GeolocationData } from "../types";
 
-export default function useGetPosition(): number[] {
+export default function useGetPosition(): GeolocationData {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const [userRefused, setUserRefused] = useState(false);
 
   navigator.geolocation.getCurrentPosition((position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
+    try {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    } catch (error) {
+      console.warn(error);
+      setUserRefused(true);
+    }
   });
-  return [latitude, longitude];
+  return { latitude, longitude, userRefused };
 }
