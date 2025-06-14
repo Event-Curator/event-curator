e67c9ed81372cc2c41a54ac9c2b334a92e503ef2
Justin-K-Ellis <jlke45@gmail.com>
2025-06-14 17:56:49 +0900

    feat: show total number of events on landing page
    Fetches metadata about prefectures, adds total number of events by
    prefecure and displays in Hero component.

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import type { MetaData } from "../types";
 import heroImg from "../assets/Matsuri.jpg";
 
 export default function Hero() {
+  const [eventTotal, setEventTotal] = useState("");
+  const fallbackTotal = "countless";
+  const api = import.meta.env.VITE_API;
+
+  useEffect(() => {
+    async function getMetaData() {
+      try {
+        const response = await fetch(`${api}/meta?key=placeProvince`);
+        if (!response.ok) {
+          console.error(response);
+          setEventTotal(fallbackTotal);
+        }
+        const data: MetaData[] = await response.json();
+        const total = data.reduce((acc, current) => acc + current.count, 0);
+        setEventTotal(total.toString());
+      } catch (error) {
+        console.error(error);
+        setEventTotal(fallbackTotal);
+      }
+    }
+    getMetaData();
+  }, [api]);
+
   return (
     <section className="relative w-full h-80 md:h-[20rem] flex items-center justify-center bg-blue-900 mb-6">
       <img
@@ -9,15 +34,15 @@ export default function Hero() {
         className="absolute inset-0 w-full h-full object-cover"
         draggable={false}
       />
-      
+
       <div className="relative z-10 text-white text-center px-4">
         <h1 className="text-6xl md:text-6xl font-extrabold drop-shadow mb-3 font-sans tracking-tight text-red-outline">
           Events in Japan
-          </h1>
-          <p className="text-2xl md:text-2xl drop-shadow mb-3 font-sans tracking-tight text-red-outline">
-            Discover & Share Festivals, Art, Concerts, Sports & More!
-            </p>
-            </div>
+        </h1>
+        <p className="text-2xl md:text-2xl drop-shadow mb-3 font-sans tracking-tight text-red-outline">
+          Search from {eventTotal} Festivals, Art, Concerts, Sports & More!
+        </p>
+      </div>
     </section>
   );
 }
